ce471632cf88edee20fa5a578309f514ae7306a1
Andrea Gonzato <gonzatoandrea98@gmail.com>
2022-05-17 12:16:20 +0200

    add onClick event for the two button male and female

--- app/src/main/java/it/units/alcoholestimator/MainActivity.java
+++ app/src/main/java/it/units/alcoholestimator/MainActivity.java
@@ -3,6 +3,12 @@ package it.units.alcoholestimator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import it.units.alcoholestimator.logic.Gender;
+import it.units.alcoholestimator.logic.User;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button maleButton = findViewById(R.id.male);
+        maleButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                User.setGender(Gender.MALE);
+                Log.i("TEST", "is male");
+            }
+        });
+
+        Button femaleButton = findViewById(R.id.female);
+        femaleButton.setOnClickListener(v -> User.setGender(Gender.FEMALE));
+
     }
+
 }
