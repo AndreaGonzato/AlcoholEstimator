6a5eda85cb773b2633a9b2526b151e484b12e25c
Andrea Gonzato <gonzatoandrea98@gmail.com>
2022-05-24 17:48:10 +0200

    add the weightPicker and a textView

--- app/src/main/java/it/units/alcoholestimator/MainActivity.java
+++ app/src/main/java/it/units/alcoholestimator/MainActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
 
 import it.units.alcoholestimator.logic.Gender;
 import it.units.alcoholestimator.logic.User;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NumberPicker numberPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,17 @@ public class MainActivity extends AppCompatActivity {
 
         Button femaleButton = findViewById(R.id.female);
         femaleButton.setOnClickListener(v -> User.setGender(Gender.FEMALE));
+
+        numberPicker = (NumberPicker) findViewById(R.id.weightPicker);
+        numberPicker.setMaxValue(200);
+        numberPicker.setMinValue(10);
+        numberPicker.setValue(70);
+        numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int oldValue, int newValue) {
+                Log.i("TEST", String.valueOf(newValue));
+            }
+        });
 
     }
 
