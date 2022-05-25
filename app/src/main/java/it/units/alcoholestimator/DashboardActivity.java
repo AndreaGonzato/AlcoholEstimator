d5ebbe32943f5b47d99435b1c7672c99e5e7fbd8
Andrea Gonzato <gonzatoandrea98@gmail.com>
2022-05-25 16:34:36 +0200

    remove action bar in Dashboard Activity

--- app/src/main/java/it/units/alcoholestimator/DashboardActivity.java
+++ app/src/main/java/it/units/alcoholestimator/DashboardActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class DashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        // remove the action bar
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+
     }
 }
