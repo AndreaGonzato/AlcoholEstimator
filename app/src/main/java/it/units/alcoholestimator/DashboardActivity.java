211f85494f9a9bc6e92bac6418d6d4991ad2d8d3
Andrea Gonzato <gonzatoandrea98@gmail.com>
2022-05-25 11:06:45 +0200

    add activity dashboard

---
+++ app/src/main/java/it/units/alcoholestimator/DashboardActivity.java
@@ -0,0 +1,14 @@
+package it.units.alcoholestimator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
--- app/src/main/java/it/units/alcoholestimator/MainActivity.java
+++ app/src/main/java/it/units/alcoholestimator/MainActivity.java
@@ -2,6 +2,7 @@ package it.units.alcoholestimator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,13 +15,19 @@ import it.units.alcoholestimator.logic.User;
 public class MainActivity extends AppCompatActivity {
 
     private NumberPicker numberPicker;
+    private Button maleButton;
+    private Button femaleButton;
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button maleButton = findViewById(R.id.male);
+        maleButton = findViewById(R.id.male);
+        saveButton = findViewById(R.id.saveButton);
+        femaleButton = findViewById(R.id.female);
+
         maleButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 User.setGender(Gender.MALE);
@@ -28,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button femaleButton = findViewById(R.id.female);
         femaleButton.setOnClickListener(v -> User.setGender(Gender.FEMALE));
 
         numberPicker = (NumberPicker) findViewById(R.id.weightPicker);
@@ -41,6 +47,15 @@ public class MainActivity extends AppCompatActivity {
             public void onValueChange(NumberPicker numberPicker, int oldValue, int newValue) {
                 // update the user weight
                 User.setWeight(newValue);
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (User.getGender() != null){
+                    startActivity(new Intent(MainActivity.this, DashboardActivity.class));
+                }
             }
         });
 
