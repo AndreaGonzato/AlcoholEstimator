2761f5e4f39d7f5e1c6543977a647d7bcb0a0fec
Andrea Gonzato <gonzatoandrea98@gmail.com>
2022-05-27 11:07:19 +0200

    AddDrinkFragment: sketch for how to handle all the button in groups extract some strings res

--- app/src/main/java/it/units/alcoholestimator/fragments/AddDrinkFragment.java
+++ app/src/main/java/it/units/alcoholestimator/fragments/AddDrinkFragment.java
@@ -1,12 +1,20 @@
 package it.units.alcoholestimator.fragments;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.Arrays;
 
 import it.units.alcoholestimator.R;
 
@@ -62,5 +70,24 @@ public class AddDrinkFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_drink, container, false);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button[] lightBeerButtons = new Button[3];
+        lightBeerButtons[0] = requireView().findViewById(R.id.lightBeerButton);
+        lightBeerButtons[1] = requireView().findViewById(R.id.lightBeerAlcoholButton);
+        lightBeerButtons[2] = requireView().findViewById(R.id.lightBeerSizeButton);
+        Arrays.stream(lightBeerButtons).forEach(button -> button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO continue here and remove this Log
+                Log.i("TEST", "clicked light beer");
+            }
+        }));
+
     }
 }
