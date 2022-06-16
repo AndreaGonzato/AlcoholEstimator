219b2d8a30eb9bc38bb70374051e953c54cfdda5
Andrea Gonzato <gonzatoandrea98@gmail.com>
2022-06-16 16:08:18 +0200

    add a calendarView that work well

--- app/src/main/java/it/units/alcoholestimator/fragments/AddDrinkFragment.java
+++ app/src/main/java/it/units/alcoholestimator/fragments/AddDrinkFragment.java
@@ -1,5 +1,7 @@
 package it.units.alcoholestimator.fragments;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,7 +12,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 import it.units.alcoholestimator.R;
 
@@ -24,6 +30,9 @@ public class AddDrinkFragment extends Fragment {
     public static final String DRINK_TYPE_KEY = "DRINK_TYPE";
     public static final String ALCOHOL_CONTENT_KEY = "ALCOHOL_CONTENT";
     public static final String DRINK_SIZE_KEY = "DRINK_SIZE";
+
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton;
 
     // TODO: Rename and change types of parameters
     private String drinkType;
@@ -88,5 +97,76 @@ public class AddDrinkFragment extends Fragment {
         EditText alcoholContentEditText = requireView().findViewById(R.id.editTextNumberAlcoholContent);
         alcoholContentEditText.setText(alcoholContentString);
 
+        initDatePicker();
+        Button dateButton = requireView().findViewById(R.id.datePickerButton);
+        dateButton.setText(getTodayDate());
+
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePickerDialog.show();
+            }
+        });
+        
+
     }
+
+    private void initDatePicker()
+    {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                Button button = requireView().findViewById(R.id.datePickerButton);
+                button.setText(date);
+            }
+        };
+
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(getContext(), style, dateSetListener, year, month, day);
+        //datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+
+    }
+
+    private String getTodayDate(){
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        month = month + 1;
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
+    private String makeDateString(int day, int month, int year) {
+        return getMonthFormat(month) + " " + day + " " +year;
+    }
+
+    private String getMonthFormat(int month) {
+        // TODO think if this is the best way to do that
+        switch (month){
+            case 1: return "JAN";
+            case 2: return "FEB";
+            case 3: return "MAR";
+            case 4: return "APR";
+            case 5: return "MAY";
+            case 6: return "JUN";
+            case 7: return "JUL";
+            case 8: return "AUG";
+            case 9: return "SET";
+            case 10: return "OCT";
+            case 11: return "NOV";
+            case 12: return "DEC";
+        }
+        return "JAN";
+    }
+
 }
