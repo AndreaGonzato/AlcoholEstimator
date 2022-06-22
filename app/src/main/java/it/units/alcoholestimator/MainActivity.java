51f5edc4f4f50ff8b85e232c0d2959156ff095c1
Andrea Gonzato <gonzatoandrea98@gmail.com>
2022-06-22 18:53:37 +0200

    add Google OAuth

--- app/src/main/java/it/units/alcoholestimator/MainActivity.java
+++ app/src/main/java/it/units/alcoholestimator/MainActivity.java
@@ -1,17 +1,29 @@
 package it.units.alcoholestimator;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.NumberPicker;
 import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import java.util.Objects;
 
@@ -27,10 +39,33 @@ public class MainActivity extends AppCompatActivity {
     private Button femaleButton;
     private Button saveButton;
 
+    private static final int RC_SIGN_IN = 9001;
+    private GoogleSignInClient googleSignInClient;
+
     // constants
     private static final float WEIGHT_FOR_SEX_SELECTED = 2.0f;
     private static final int SCALAR_PIXEL_SIZE_FOR_PRESSED_BUTTON_SEX = 25;
     private static final int SCALAR_PIXEL_SIZE_FOR_UNPRESSED_BUTTON_SEX = 14;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        if(account == null){
+            // first time that the user is doing the log in
+            Log.i("TEST", "welcome for the first time");
+        }else {
+            // user has already log in
+            Log.i("TEST", "welcome again");
+            Log.i("TEST", ""+account.getEmail());
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +74,26 @@ public class MainActivity extends AppCompatActivity {
 
         // remove the action bar
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        // Set the dimensions of the sign-in button.
+        SignInButton signInGoogleButton = findViewById(R.id.signInButton);
+        signInGoogleButton.setSize(SignInButton.SIZE_STANDARD);
+        signInGoogleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+
+
+
+
+
+        GoogleSignInOptions googleOptions =
+                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestEmail()
+                        .build();
+        googleSignInClient = GoogleSignIn.getClient(this, googleOptions);
 
         maleButton = findViewById(R.id.male);
         saveButton = findViewById(R.id.saveButton);
@@ -130,6 +185,40 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    private void signIn() {
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
     }
 
     public static int getScreenWidth() {
